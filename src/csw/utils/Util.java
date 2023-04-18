9e2cf19074bb0a82f139fee47612cb084e8f3814
Student <ashutoshmaha2909@gmail.com>
2023-04-18 10:19:09 +0530

    added all code

--- src/csw/utils/Util.java
+++ src/csw/utils/Util.java
@@ -1,35 +1,52 @@
 package utils;
+
+import java.util.Scanner;
+
 public class Util {
-    static public<T> void display(T[] arr, String msg){
-        String s = "";
-        for (int i=0; i<arr.length; i++){
-            if (i == (arr.length-1)){
-                s += arr[i];
-            } else {
-                s+= arr[i]+", ";
-            }
+    static Scanner sc = new Scanner(System.in);
+
+    static public int[] takeUserInput() {
+        System.out.println("Enter the array elements seprated by spaces: ");
+        String inputs = sc.nextLine();
+        String[] strArr = inputs.split(" ");
+        int[] arr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
         }
-        System.out.println(msg + s);
+        return arr;
     }
-    static public void display(int[] arr, String msg){
+
+    static public <T> void display(T[] arr, String msg) {
         String s = "";
-        for (int i=0; i<arr.length; i++){
-            if (i == (arr.length-1)){
+        for (int i = 0; i < arr.length; i++) {
+            if (i == (arr.length - 1)) {
                 s += arr[i];
             } else {
-                s+= arr[i]+", ";
+                s += arr[i] + ", ";
             }
         }
         System.out.println(msg + s);
     }
 
-    static public<T> void swap(T[] arr, int i, int j){
+    static public void display(int[] arr, String msg) {
+        String s = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (i == (arr.length - 1)) {
+                s += arr[i];
+            } else {
+                s += arr[i] + ", ";
+            }
+        }
+        System.out.println(msg + s);
+    }
+
+    static public <T> void swap(T[] arr, int i, int j) {
         T temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    static public void swap(int[] arr, int i, int j){
+    static public void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
