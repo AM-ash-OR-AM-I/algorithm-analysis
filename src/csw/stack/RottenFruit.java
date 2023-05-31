f04ad295e2b83ea1bc9e288c17c91c78814d33fb
Student <ashutoshmaha2909@gmail.com>
2023-05-31 11:50:18 +0530

    added code

---
+++ src/csw/stack/RottenFruit.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class RottenFruit {
+    
+    public static void main(String[] args) {
+        
+    }
+}
--- src/csw/stack/StockSpanProblem.java
+++ src/csw/stack/StockSpanProblem.java
@@ -1,31 +1,47 @@
 package stack;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 import utils.Util;
 
 public class StockSpanProblem {
-    static int stockSpanRange(int[] arr) {
+    static int[] stockSpanRange(int[] arr) {
         /*
          * O(N^2)
          */
-        int maxSpan = 1;
+        int[] SR = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             int currentStock = arr[i];
+            SR[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (currentStock > arr[j]) {
-                    maxSpan = (i - j) + 1;
+                    SR[i] = (i - j) + 1;
                     break;
                 }
             }
         }
-        return maxSpan;
+        return SR;
     }
 
-    static int stockSpanRangeOptimized(int[] arr){
-
+    static int[] stockSpanRangeOptimized(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] SR = new int[arr.length];
+        stack.push(0);
+        SR[0] = 1;
+        for (int i = 1; i < SR.length; i++) {
+            while (!stack.isEmpty() && arr[i] >= arr[stack.peek()]) {
+                stack.pop();
+            }
+            SR[i] = (stack.isEmpty()) ? (i + 1) : (i - stack.peek());
+            stack.push(i);
+        }
+        return SR;
     }
+
     public static void main(String[] args) {
         int[] arr = Util.takeUserInput();
-        int maxSpan = stockSpanRange(arr);
-        System.out.println(maxSpan);
+        int[] maxSpan = stockSpanRangeOptimized(arr);
+        System.out.println(Arrays.toString(maxSpan));
     }
 }
