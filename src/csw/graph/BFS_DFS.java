ed51470c67a23da6d945ffa8fd9a79ad739b13b1
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-26 16:06:09 +0530

    updatde code for bfs dfs

--- src/csw/graph/BFS_DFS.java
+++ src/csw/graph/BFS_DFS.java
@@ -1,7 +1,6 @@
-package ASSIGNMENT2A;
+package graph;
 
 import java.util.ArrayDeque;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -65,35 +64,25 @@ class Graph {
       }
     }
   }
-}
 
-// Traverse using BFS
-public class Q5_Q6 {
-  public static void main(String[] args) {
-    Graph graph = new Graph(11);
+  public class BFS_DFS {
+    public static void main(String[] args) {
+      Graph graph = new Graph(11);
+      graph.addEdge(0, 1);
+      graph.addEdge(0, 7);
+      graph.addEdge(0, 10);
+      graph.addEdge(1, 2);
+      graph.addEdge(1, 4);
+      graph.addEdge(2, 3);
+      graph.addEdge(2, 5);
+      graph.addEdge(5, 6);
+      graph.addEdge(7, 8);
+      graph.addEdge(7, 9);
 
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 7);
-    graph.addEdge(0, 10);
-    graph.addEdge(1, 2);
-    graph.addEdge(1, 4);
-    graph.addEdge(2, 3);
-    graph.addEdge(2, 5);
-    graph.addEdge(5, 6);
-    graph.addEdge(7, 8);
-    graph.addEdge(7, 9);
-
-    System.out.println("BFS Traversal of the graph:");
-    graph.BFS(0);
-    System.out.println("\nDFS Traversal of the graph:");
-    graph.DFS(0);
+      System.out.println("BFS Traversal of the graph:");
+      graph.BFS(0);
+      System.out.println("\nDFS Traversal of the graph:");
+      graph.DFS(0);
+    }
   }
 }
-
-/*
- * O/P:
- * BFS Traversal of the graph:
- * 2 1 3 0
- * DFS Traversal of the graph:
- * 2 3 1 0
- */
---
+++ src/csw/graph/PrimsAlgorithm.java
@@ -0,0 +1,12 @@
+package graph;
+
+// Create MST using prim's algorithm.
+public class PrimsAlgorithm {
+  static void createMST() {
+    // TODO: Do this code.
+    
+  }
+  public static void main(String[] args) {
+
+  }
+}
