39f36a816d6be9b52bd4f893aea67358732bd3d9
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-28 11:00:39 +0530

    added optimized select largest

--- src/sorting/select_randomised.py
+++ src/sorting/select_randomised.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 def optimized_select(s: List[int], k: int) -> int:
+    """
+    Returns the kth smallest element in s.
+    """
     global iterations
     pivot = s[randint(0, len(s) - 1)]
     s1 = []
@@ -16,20 +19,29 @@ def optimized_select(s: List[int], k: int) -> int:
             s1.append(x)
         elif x > pivot:
             s2.append(x)
-    if len(s1) < k - 1:
-        return optimized_select(s2, k - len(s1) - 1)
+    if (l := len(s1)) < k - 1:
+        return optimized_select(s2, k - l - 1)
     elif len(s1) == k - 1:
         return pivot
     elif len(s1) > k - 1:
         return optimized_select(s1, k)
 
 
-def quick_select(arr: List[int], begin, end, k: int) -> int:
+def optimized_select_largest(s: List[int], k: int) -> int:
+    """
+    Returns the kth largest element in s.
+    """
+    return optimized_select(s, len(s) - k + 1)
+
+
+def quick_select(arr: List[int], begin: int, end: int, k: int) -> int:
     def swap(i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
     def partition() -> int:
         global iterations
+        rand_index = randint(begin, end)
+        swap(rand_index, end)
         pivot = arr[end]
         i = begin - 1
         for j in range(begin, end):
@@ -53,30 +65,41 @@ def quick_select(arr: List[int], begin, end, k: int) -> int:
         return arr[begin]
 
 
+def run_all_testcases():
+    global iterations
+    for _ in range(1):
+        a = np.random.permutation(10**5)
+        n = len(a)
+        print(f"{n = }")
+        for _ in range(3):
+            k = randint(1, n)
+            iterations = 0
+            element1 = optimized_select(a, k)
+            ratio1 = iterations / n
+            optimized_select_ratio.append(ratio1)
+            iterations = 0
+            element2 = quick_select(a, 0, n - 1, k)
+            ratio2 = iterations / n
+            quick_select_ratio.append(ratio2)
+            assert element1 == element2
+    print(f"{optimized_select_ratio = }")
+    print(f"{quick_select_ratio = }")
+    print(f"{np.mean(optimized_select_ratio) = }")
+    print(f"{np.mean(quick_select_ratio) = }")
+    # print(f"{np.std(coefficent_arr) = }")
+    # print(f"{np.std(log_ratio) = }")
+    print(f"{np.min(optimized_select_ratio) = }")
+    print(f"{np.max(optimized_select_ratio) = }")
+    print(f"{np.min(quick_select_ratio) = }")
+    print(f"{np.max(quick_select_ratio) = }")
+
+
 optimized_select_ratio = []
 quick_select_ratio = []
-for _ in range(1):
+
+if __name__ == "__main__":
+    iterations = 0
+    # run_all_testcases()
     a = np.random.permutation(10**5)
-    n = len(a)
-    print(f"{n = }")
-    for _ in range(3):
-        k = randint(1, n)
-        iterations = 0
-        element1 = optimized_select(a, k)
-        ratio1 = iterations / n
-        optimized_select_ratio.append(ratio1)
-        iterations = 0
-        element2 = quick_select(a, 0, n - 1, k)
-        ratio2 = iterations / n
-        quick_select_ratio.append(ratio2)
-        assert element1 == element2
-print(f"{optimized_select_ratio = }")
-print(f"{quick_select_ratio = }")
-print(f"{np.mean(optimized_select_ratio) = }")
-print(f"{np.mean(quick_select_ratio) = }")
-# print(f"{np.std(coefficent_arr) = }")
-# print(f"{np.std(log_ratio) = }")
-print(f"{np.min(optimized_select_ratio) = }")
-print(f"{np.max(optimized_select_ratio) = }")
-print(f"{np.min(quick_select_ratio) = }")
-print(f"{np.max(quick_select_ratio) = }")
+    print(a)
+    print(optimized_select_largest(a, 11000))
