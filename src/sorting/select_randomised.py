df4d52db9fd83ff20f86ff1560f295c2398ea516
Ashutosh <ashutosh2gudu@gmail.com>
2023-06-28 13:15:45 +0530

    added code for optimizsed select randomised

--- src/sorting/select_randomised.py
+++ src/sorting/select_randomised.py
@@ -1,11 +1,12 @@
 import math
 from random import randint
+from timeit import timeit
 from typing import List
 
 import numpy as np
 
 
-def optimized_select(s: List[int], k: int) -> int:
+def select_randomised(s: List[int], k: int) -> int:
     """
     Returns the kth smallest element in s.
     """
@@ -20,21 +21,58 @@ def optimized_select(s: List[int], k: int) -> int:
         elif x > pivot:
             s2.append(x)
     if (l := len(s1)) < k - 1:
-        return optimized_select(s2, k - l - 1)
+        return select_randomised(s2, k - l - 1)
     elif len(s1) == k - 1:
         return pivot
     elif len(s1) > k - 1:
-        return optimized_select(s1, k)
+        return select_randomised(s1, k)
 
 
-def optimized_select_largest(s: List[int], k: int) -> int:
+def optimized_select_randomised(s: List[int], k: int) -> int:
+    """
+    Returns the kth smallest element in s.
+    """
+    global iterations
+    n = len(s)
+
+    """
+    If the size of the partition is less than n/4, then central splitter is not found.
+    
+    The expected number of trials before a central splitter is found is 2.
+    """
+    if len(s) <= 4:
+        return s[0]
+    while True:
+        s1 = []
+        s2 = []
+        pivot = s[randint(0, n - 1)]
+        for x in s:
+            iterations += 1
+            if x < pivot:
+                s1.append(x)
+            elif x > pivot:
+                s2.append(x)
+        if len(s1) > n / 4 and len(s2) > n / 4:
+            break
+
+    n1, n2 = len(s1), len(s2)
+
+    if n1 < k - 1:
+        return optimized_select_randomised(s2, k - n1 - 1)
+    elif n1 == k - 1:
+        return pivot
+    elif n1 > k - 1:
+        return optimized_select_randomised(s1, k)
+
+
+def select_largest(s: List[int], k: int) -> int:
     """
     Returns the kth largest element in s.
     """
-    return optimized_select(s, len(s) - k + 1)
+    return select_randomised(s, len(s) - k + 1)
 
 
-def quick_select(arr: List[int], begin: int, end: int, k: int) -> int:
+def quick_select_randomised(arr: List[int], begin: int, end: int, k: int) -> int:
     def swap(i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
@@ -58,48 +96,60 @@ def quick_select(arr: List[int], begin: int, end: int, k: int) -> int:
         if index == k - 1:
             return arr[index]
         elif index < k - 1:
-            return quick_select(arr, index + 1, end, k)
+            return quick_select_randomised(arr, index + 1, end, k)
         else:
-            return quick_select(arr, begin, index - 1, k)
+            return quick_select_randomised(arr, begin, index - 1, k)
     else:
         return arr[begin]
 
 
 def run_all_testcases():
+    """
+    NOTE: From my observations, the `select_randomised()` function even though technically,
+    less efficient than the `optimized_select_randomised()` function, it seems that worst case
+    scenarios are very rare. Hence, the `optimized_select_randomised()` function is not showing
+    any significant improvement over the `select_randomised()` function.
+    """
     global iterations
-    for _ in range(1):
-        a = np.random.permutation(10**5)
+    for _ in range(1000):
+        a = np.random.randint(0, 10**6, size=200)
+        a = list(set(a))
         n = len(a)
-        print(f"{n = }")
-        for _ in range(3):
+        for _ in range(20):
             k = randint(1, n)
-            iterations = 0
-            element1 = optimized_select(a, k)
-            ratio1 = iterations / n
-            optimized_select_ratio.append(ratio1)
-            iterations = 0
-            element2 = quick_select(a, 0, n - 1, k)
-            ratio2 = iterations / n
-            quick_select_ratio.append(ratio2)
-            assert element1 == element2
-    print(f"{optimized_select_ratio = }")
-    print(f"{quick_select_ratio = }")
-    print(f"{np.mean(optimized_select_ratio) = }")
-    print(f"{np.mean(quick_select_ratio) = }")
-    # print(f"{np.std(coefficent_arr) = }")
-    # print(f"{np.std(log_ratio) = }")
-    print(f"{np.min(optimized_select_ratio) = }")
-    print(f"{np.max(optimized_select_ratio) = }")
-    print(f"{np.min(quick_select_ratio) = }")
-    print(f"{np.max(quick_select_ratio) = }")
+
+            optimized_time = timeit(lambda: optimized_select_randomised(a, k), number=2)
+            optimized_select_times.append(optimized_time)
+
+            select_time = timeit(lambda: select_randomised(a, k), number=2)
+            select_times.append(select_time)
+
+            quick_select_time = timeit(
+                lambda: quick_select_randomised(a, 0, n - 1, k), number=2
+            )
+            quick_select_times.append(quick_select_time)
+
+    # print(f"{select_ratio = }")
+    # print(f"{optimized_select_ratio = }")
+    # print(f"{quick_select_ratio = }")
+    # print(f"{np.mean(select_ratio) = }")
+    # print(f"{np.mean(optimized_select_ratio) = }")
+    # print(f"{np.mean(quick_select_ratio) = }")
+    # print(f"{np.min(select_ratio) = }")
+    print(f"{np.max(select_times) = }")
+    print(f"{np.max(optimized_select_times) = }")
+    print(f"{np.max(quick_select_times) = }")
+    print(f"{np.max(select_times)-np.min(select_times) = }")
+    print(f"{np.max(optimized_select_times)-np.min(optimized_select_times) = }")
+    print(f"{np.max(quick_select_times)-np.min(quick_select_times) = }")
+    # print(f"{np.min(optimized_select_ratio) = }")
+    # print(f"{np.min(quick_select_ratio) = }")
 
 
-optimized_select_ratio = []
-quick_select_ratio = []
+select_times = []
+optimized_select_times = []
+quick_select_times = []
 
 if __name__ == "__main__":
     iterations = 0
-    # run_all_testcases()
-    a = np.random.permutation(10**5)
-    print(a)
-    print(optimized_select_largest(a, 11000))
+    run_all_testcases()
